62898faaf26f418b164c30c4ec9bd597d01c94af
jourdannf <jourdann.fraser@gmail.com>
2024-05-30 19:43:31 -0400

    Finished weighted average

--- javascript_fundamentals.js
+++ javascript_fundamentals.js
@@ -96,10 +96,77 @@ const CourseInfo = {
 
     //Assign id key and push learner onto the learnerCoursesScores
     ids.forEach((num) => {
-        console.log(learner);
+        // console.log(learner);
         learner.id = num;
         learnerCourseScores.push({...learner});
     });
+
+    //Get totalScores from learnerSubmission
+    //Get totalPossibleScores from assignmentGroup.submission
+    //Divide the two values and put it as the avg key in each learner person
+
+    let completedAssignByLearner = {}; // all assignments each user completed; 
+                                        //key is id and value is array of asssignments completed
+
+    //After this code, all the averages should be the total of all the scores from each learner
+    learnerSubmission.forEach((person)=> {
+
+        //Get the ids for the assignments each learner completed
+        //If learner_id is not a key in completedAssignByLearner, then make an empty array
+        if (!Object.keys(completedAssignByLearner).includes(String(person.learner_id))){
+            completedAssignByLearner[person.learner_id] = [];
+        }
+        completedAssignByLearner[person.learner_id].push(person.assignment_id);
+
+        //Add the scores for each assignment per learner
+        for (let i = 0; i < learnerCourseScores.length; i ++){
+            if (!Object.keys(learnerCourseScores[i]).includes("avg")){
+                learnerCourseScores[i].avg = 0;
+            }
+            
+            if(person.learner_id === learnerCourseScores[i].id){
+                learnerCourseScores[i].avg += person.submission.score;
+                break;
+            }
+        }
+    });
+
+    // console.log(completedAssignByLearner);
+
+    
+
+    //The amount of total points possible for each learner
+
+    let allAssignments = assignmentGroup.assignments; //array of all the assignments
+
+    learnerCourseScores.forEach((learnerGrades) => {
+        //Total of all possible scores from each assignment
+        let totalScores = 0;
+
+        let assignmentsDone = completedAssignByLearner[learnerGrades.id];
+
+        assignmentsDone.forEach((id)=> {
+            // for each id find the matching assignment
+            
+            for (let i = 0; i < allAssignments.length; i++){
+                if (allAssignments[i].id === id){
+                    totalScores += allAssignments[i].points_possible;
+                    break;
+                }
+            }
+            
+        });
+        
+        learnerGrades.avg /=totalScores;
+    });
+
+    // assignmentGroup.assignments.forEach((assignment) => {
+    //     totalScores += assignment.points_possible;
+    // })
+
+    // learnerCourseScores.forEach((person) => person.avg/=totalScores);
+
+    //Divide and add that to the object
 
     console.log(learnerCourseScores)
     
